63e86f6aa2ef0fd2c796dca2c9d2aaff17fb4480
Tuan Nguyen <famaxlevel1997@gmail.com>
2023-09-02 00:53:10 +0700

    feat: products show - handle api

--- constants/api.ts
+++ constants/api.ts
@@ -3,3 +3,4 @@ export const PROFILE_UPDATE_API = '/users/profile/update'
 export const PROFILE_API = '/users/profile'
 export const LIST_PRODUCTS_API = '/products'
 export const PRODUCT_MASTER_DATA_API = '/products/master-data'
+export const PRODUCT_DETAIL_API = (id: number) => `/products/${id}`
