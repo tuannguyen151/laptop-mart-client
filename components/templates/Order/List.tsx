d2e41fe357ec3cc2201fc5090ef51ece63112004
Tuan Nguyen <famaxlevel1997@gmail.com>
2023-09-03 11:30:10 +0700

    feat: order list

---
+++ components/templates/Order/List.tsx
@@ -0,0 +1,11 @@
+import ListOrderItems from '@/components/organisms/Order/ListOrderItems'
+
+const ListTemplate = () => {
+  return (
+    <div className='my-4'>
+      <ListOrderItems />
+    </div>
+  )
+}
+
+export default ListTemplate
